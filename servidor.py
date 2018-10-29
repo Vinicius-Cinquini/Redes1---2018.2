c18d1c7fd8090af35f8a16a1296638046b8cf800
Vinicius-Cinquini <vini.wolfling@gmail.com>
2018-10-29 14:28:43 -0300

    Entregar

--- servidor.py
+++ servidor.py
@@ -5,23 +5,12 @@ from registros import registro
 from flask import Flask
 app = Flask(__name__)
 
-<<<<<<< HEAD
 app = Flask(__name__, static_url_path = "/Users/Vinny/Projects/Python/Redes1---2018.2", static_folder = "/Users/Vinny/Projects/Python/Redes1---2018.2")
-=======
-#@app.route("/")
-
-hoje = datetime.datetime.now()
-request = open("request.txt", "r")
-cpf = request.readline()[15:-1]
-password = request.readline()[15:-1]
->>>>>>> d0229107ca449e1ae334fed70e1c14551c09a0b1
 
 @app.route("/foto/")
 def foto3x4():
-<<<<<<< HEAD
 	return render_template("F"+dre+".png", mimetype='image/gif')
-
-=======
+	
 	foto = Image.open("F"+cpf+".png")
 	byteArray = io.BytesIO()
 	foto.save(byteArray, format="PNG")
@@ -29,17 +18,6 @@ def foto3x4():
 	return byteArray
 
 
-"""def montarMensagem(registro):
-        dre = sys.argv[1]
-        mensagem =              "STATUS         0 OK\n"
-        mensagem = mensagem +   "NOME           "+registro[dre][0]+"\n"
-        mensagem = mensagem +   "CURSO          "+registro[dre][1]+"\n"
-        mensagem = mensagem +   "CPF            "+registro[dre][2]+"\n"
-        mensagem = mensagem +   "NASCIMENTO     "+registro[dre][3]+"\n"
-        mensagem = mensagem +   "FOTO           "+str(foto3x4())+"\n\n"
-        print (mensagem)
-"""
->>>>>>> d0229107ca449e1ae334fed70e1c14551c09a0b1
 def montarArquivo(registro):
         #dre = sys.argv[1]
         arq = open("resposta.txt","w+")
@@ -56,7 +34,6 @@ def montarFalhaAut():
     arq.write("STATUS          1 FALHA DE AUTENTICAÇÃO\n")
     arq.close()
 
-<<<<<<< HEAD
 def makeQRC(cpf):
         qrSize = 450
         qrc = qrcode.make(cpf)
@@ -67,14 +44,11 @@ def makeQRC(cpf):
 def home():
 	return render_template("index.html")
 
-#@app.route('/action_page.php?cpf=<cpf>&senha=<password>')#, methods=['GET','POST'])
 @app.route('/documento', methods=['POST'])
 def documento():
 	cpf = request.form.get('cpf')
 	password = request.form.get('senha')
 	
-	#print("CPF = "+str(cpf)+"\nSenha="+str(password)+"\n\n")
-
 	if (password == registro[cpf][4]):
 		nome=registro[cpf][0]
 		curso=registro[cpf][1]
@@ -92,9 +66,7 @@ def documento():
 	else:
 		return render_template("erro.html")
 
-if __name__ == "__main__":
-	app.run(debug=true, port=5000)
-=======
+
 def montarFalhaCPF():
     arq = open("resposta.txt","w+")
     arq.write("STATUS          1 FALHA DE AUTENTICAÇÃO\n")
@@ -109,8 +81,5 @@ def main():
     else:
         montarFalhaAut()
 
-#montarMensagem(registro)
-#montarArquivo(registro)
-
-#main()
->>>>>>> d0229107ca449e1ae334fed70e1c14551c09a0b1
+if __name__ == "__main__":
+	app.run(debug=true, port=5000)
